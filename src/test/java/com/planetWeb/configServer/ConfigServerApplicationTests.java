6a14f8d123d06274832dc4263d2ebb81216d503f
Dasun_105089 <dasun.wahalathanthrige@axiatadigitallabs.com>
2022-07-18 13:39:47 +0530

    config server initial commit

---
+++ src/test/java/com/planetWeb/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetWeb.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
